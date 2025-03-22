74df991637c53f9aa4272c56386f3042dd93dac2
patryk <rpatryk2302@gmail.com>
2025-03-22 17:04:49 +0100

    Reapply "Adde new feature for admin"
    This reverts commit 77701ebb971fbaea46355300abca261e24e99987.

---
+++ app.Services.AuthAPI/Service/UserService.cs
@@ -0,0 +1,80 @@
+﻿using app.Services.AuthAPI.Models.DTO;
+using app.Services.AuthAPI.Repository.Interface;
+using app.Services.AuthAPI.Service.IService;
+using AutoMapper;
+using System.Data.Common;
+
+namespace app.Services.AuthAPI.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+        private readonly ResponseDTO _response;
+
+        public UserService(IMapper mapper, IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+            _response = new ResponseDTO();  
+        }
+
+        public async Task<ResponseDTO> DeleteUser(string userId)
+        {
+            try
+            {
+                var findUser = await _userRepository.FindUser(userId);
+
+                if (findUser == null)
+                {
+                    _response.IsSuccess = false;
+
+                    _response.Message = "User not found";
+                }
+
+                await _userRepository.DeleteUser(findUser);
+            }
+            catch (DbException dbEx)
+            {
+                _response.IsSuccess = false;
+
+                _response.Message = dbEx.Message;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+
+                _response.Message = ex.Message;
+            }
+
+
+            return _response;
+           
+        }
+
+        public async Task<ResponseDTO> GetUsers()
+        {
+            try
+            {
+                var users = await _userRepository.GetAllUsers();
+
+                _response.Result =  users.Select(u => _mapper.Map<GetUserDTO>(u)).ToList();
+                
+            }
+            catch(DbException dbEx)
+            {
+                _response.IsSuccess = false;
+
+                _response.Message = dbEx.Message;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ app.Web/Commands/UserCommands/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using app.Web.Models.DTO;
+using MediatR;
+
+namespace app.Web.Commands.UserCommands
+{
+    public record DeleteUserCommand(string Id) : IRequest<ResponseDTO>;
+    
+    
+}
